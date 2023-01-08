b1b545f2842e5c79f029338db68be6a36f63f144
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-01-08 21:02:48 +0200

    Update counter.v

--- RTL/counter.v
+++ RTL/counter.v
@@ -18,11 +18,13 @@ module counter(
   );
 
 
-always@(posedge clk or negedge rst)
+always@(posedge clk or negedge rst)	// When will Always Block Be Triggered
 begin
 	if(!rst)
+		// How Output reacts when Reset Is Asserted
 		count=0;
 	else
+		// How Output reacts when Rising Edge of Clock Arrives?
 		count=count+1;
 end
 endmodule
